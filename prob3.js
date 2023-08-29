cb61923c4c67af6b52003985d0a660c0a35dca92
kirthi1594 <kirthikajayaraman1594@gmail.com>
2023-08-29 16:23:07 +0530

    JS intro

---
+++ prob3.js
@@ -0,0 +1,10 @@
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+
+
+let name ="Ranak";
+let age = 3;
+console.log(name,age)
+console.log(typeof(name),typeof(age));
---
+++ prob4.js
@@ -0,0 +1,27 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+
+
+var name = "Ranak"
+var school = "DAV"
+var grade = "I"
+var section ="D"
+var roll = "027"
+
+console.log("!-----##### RESULT #####-----!");
+console.log("!  Name    : ",name,"         !");
+console.log("!  School  : ",school,"           !");
+console.log("!  Grade   : ",grade,"             !");
+console.log("!  Section : ",section,"             !");
+console.log("!  Roll    : ",roll,"           !");
+console.log("!                            !");
+console.log("!                            !");
+
+console.log("! ***** Marks Obtained ***** !");
+console.log("!Lang-98 Maths-100 Science-99!");
+console.log("!----------------------------!");
+
+
+
